3929fefdfe9230b2fc21a984c5373a57123aaa10
Maeve Kennedy <me@co60.ca>
2018-08-30 13:57:48 -0400

    Temp remove cookie

--- metrics.go
+++ metrics.go
@@ -390,7 +390,7 @@ func MetricStart(metrics *MetricsParameters) {
 	mux.Handle("/history/short", wc.CheckCookie(cookieAction)(beaconShortHistory()))
 	mux.Handle("/history/trilateration", wc.CheckCookie(cookieAction)(beaconTrilateration()))
 
-	mux.Handle("/history/maptracking", wc.CheckCookie(cookieAction)(filteredMapLocation(mp)))
+	mux.Handle("/history/maptracking", /*wc.CheckCookie(cookieAction)*/(filteredMapLocation(mp)))
 
 	mux.Handle("/history/export", wc.CheckCookie(cookieAction)(getCSV()))
 
